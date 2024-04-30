135237ecc258923fa7416390fb8ec1d29848385d
XIYAD <118036348+Xiyad69@users.noreply.github.com>
2024-04-30 19:43:41 +0600

    Update xiyad.py

--- xiyad.py
+++ xiyad.py
@@ -10,7 +10,7 @@ try:
     import requests
 except:
     os.system('pip install requests')
-os.system('xdfileg-open https://facebook.com/Xiyad.4040.XD/')
+    os.system('xdfileg-open https://facebook.com/Xiyad.4040.XD/')
 import requests
 try:
     if sys.argv[1]=='update':
